490f6e5113b25493985d9367ab4ee9f76ddd81ce
ielg78 <ielg78@gmail.com>
2025-09-08 12:19:27 +0200

    Add files via upload
    Prosjekt opplastet

---
+++ js/produksjoner.js
@@ -0,0 +1,44 @@
+
+
+forestillinger =[
+
+{
+    dato: 2026,
+    tittel: "Dalmatinerne",
+    innhold: "Settes opp i Sandvika teater våren 2026",
+    bildemappe: "Dalmatinerne2026/",
+    bilder: [   
+                {bildefil: "Dalmatiner1.jpg", bildetekst: "", bildekreditt: ""}
+            ]             
+},
+{
+    dato: 2025,
+    tittel: "Grease",
+    innhold: "Ble satt opp på Sandvika teater mars 2025",
+    bildemappe: "Grease2025/",
+    bilder: [
+         {bildefil: "Grease1.jpg", bildetekst: "", bildekreditt: ""},
+         {bildefil: "Grease1.jpg", bildetekst: "", bildekreditt: ""}
+
+    ]              //Legg inn bildenavn med anførselstegn og komma mellom
+},
+{
+    dato: 2024,
+    tittel: "Aladdin",
+    innhold: "Ble satt opp på Sandvika teater våren 2024",
+    bildemappe: "Aladdin2024/",
+    bilder: [
+         {bildefil: "Aladdin1.jpg", bildetekst: "", bildekreditt: ""}
+    ]              //Legg inn bildenavn med anførselstegn og komma mellom
+},
+{
+    dato: 2023,
+    tittel: "Jungelboken",
+    innhold: "Ble satt opp på Sandvika teater våren 2023",
+    bildemappe: "Jungelboken2025/",
+    bilder: [
+         {bildefil: "Jungelboken1.jpg", bildetekst: "", bildekreditt: ""}
+    ]              //Legg inn bildenavn med anførselstegn og komma mellom
+}
+
+];
