ff179e79afbe38ac811dd8477b2dc7861b52d3e8
ielg78 <ielg78@gmail.com>
2025-09-09 10:39:14 +0200

    Update produksjoner.js

--- js/produksjoner.js
+++ js/produksjoner.js
@@ -6,9 +6,9 @@ forestillinger =[
     dato: 2026,
     tittel: "Dalmatinerne",
     innhold: "Settes opp i Sandvika teater våren 2026",
-    bildemappe: "Dalmatinerne2026/",
+    bildemappe: "Dalmatinerne2026/", 
     bilder: [   
-                {bildefil: "Dalmatiner1.jpg", bildetekst: "", bildekreditt: ""}
+                {bildefil: "img1.jpg", bildetekst: "", bildekreditt: "Inger Elisabeth"}
             ]             
 },
 {
@@ -17,8 +17,8 @@ forestillinger =[
     innhold: "Ble satt opp på Sandvika teater mars 2025",
     bildemappe: "Grease2025/",
     bilder: [
-         {bildefil: "Grease1.jpg", bildetekst: "", bildekreditt: ""},
-         {bildefil: "Grease1.jpg", bildetekst: "", bildekreditt: ""}
+         {bildefil: "Grease1.jpg", bildetekst: "", bildekreditt: "Nina Krømer"},
+         {bildefil: "Grease1.jpg", bildetekst: "", bildekreditt: "Nina Krømer"}
 
     ]              //Legg inn bildenavn med anførselstegn og komma mellom
 },
@@ -28,16 +28,16 @@ forestillinger =[
     innhold: "Ble satt opp på Sandvika teater våren 2024",
     bildemappe: "Aladdin2024/",
     bilder: [
-         {bildefil: "Aladdin1.jpg", bildetekst: "", bildekreditt: ""}
+         {bildefil: "Aladdin1.jpg", bildetekst: "", bildekreditt: "Nina Krømer"}
     ]              //Legg inn bildenavn med anførselstegn og komma mellom
 },
 {
     dato: 2023,
     tittel: "Jungelboken",
     innhold: "Ble satt opp på Sandvika teater våren 2023",
-    bildemappe: "Jungelboken2025/",
+    bildemappe: "Jungelboken2023/",
     bilder: [
-         {bildefil: "Jungelboken1.jpg", bildetekst: "", bildekreditt: ""}
+         {bildefil: "Jungelboken1.jpg", bildetekst: "", bildekreditt: "Nina Krømer"}
     ]              //Legg inn bildenavn med anførselstegn og komma mellom
 }
 
