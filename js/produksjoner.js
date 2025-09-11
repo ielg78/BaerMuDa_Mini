2dd5e3db1863d9280172a2fb37de7d6ba1ab1660
ielg78 <ielg78@gmail.com>
2025-09-11 20:18:41 +0200

    Update produksjoner.js
    Lagt til alle forestillingene

--- js/produksjoner.js
+++ js/produksjoner.js
@@ -2,17 +2,16 @@
 
 forestillinger =[
 
-{
-    dato: 2026,
+{   dato: 2026,
     tittel: "Dalmatinerne",
-    innhold: "Settes opp i Sandvika teater våren 2026",
+    innhold: "Kommer våren 2026",
     bildemappe: "Dalmatinerne2026/", 
     bilder: [   
                 {bildefil: "img1.jpg", bildetekst: "", bildekreditt: "Inger Elisabeth"}
-            ]             
+            ],
+    youtube: ""            
 },
-{
-    dato: 2025,
+{   dato: 2025,
     tittel: "Grease",
     innhold: "Ble satt opp på Sandvika teater mars 2025",
     bildemappe: "Grease2025/",
@@ -20,26 +19,288 @@ forestillinger =[
          {bildefil: "grease1.jpg", bildetekst: "", bildekreditt: "Nina Krømer"},
          {bildefil: "grease1.jpg", bildetekst: "", bildekreditt: "Nina Krømer"}
 
-    ]              //Legg inn bildenavn med anførselstegn og komma mellom
+    ],              //Legg inn bildenavn med anførselstegn og komma mellom
+    youtube: ""  
 },
-{
-    dato: 2024,
+{   dato: 2024,
     tittel: "Aladdin",
     innhold: "Ble satt opp på Sandvika teater våren 2024",
     bildemappe: "Aladdin2024/",
     bilder: [
          {bildefil: "aladdin1.jpg", bildetekst: "", bildekreditt: "Nina Krømer"}
-    ]              //Legg inn bildenavn med anførselstegn og komma mellom
+    ],              //Legg inn bildenavn med anførselstegn og komma mellom
+    youtube: "" 
 },
-{
-    dato: 2023,
+{   dato: 2023,
     tittel: "Jungelboken",
     innhold: "Ble satt opp på Sandvika teater våren 2023",
-    bildemappe: "Jungelboken2023/",
+    bildemappe: "jungelboken2023/",
     bilder: [
-         {bildefil: "jungelboken1.jpg", bildetekst: "", bildekreditt: "Nina Krømer"}
-    ]              //Legg inn bildenavn med anførselstegn og komma mellom
+         {bildefil: "Jungelboken1.jpg", bildetekst: "", bildekreditt: "Nina Krømer"}
+    ],              //Legg inn bildenavn med anførselstegn og komma mellom
+    youtube: "https://youtu.be/9YkW_9yIpvs?si=WKfl_pyKLFN0oLgx" 
+},
+{   dato: 2022,
+    tittel: "Annie",
+    innhold: "",
+    bildemappe: "Annie2023/",
+    bilder: [
+        /* {bildefil: "", bildetekst: "", bildekreditt: ""}*/
+    ],              //Legg inn bildenavn med anførselstegn og komma mellom
+    youtube: "" 
+},
+{   dato: 2021,
+    tittel: "Grease",
+    innhold: "Avlyst pga. pandemien",
+    bildemappe: "Grease2021/",
+    bilder: [
+        /*{bildefil: "", bildetekst: "", bildekreditt: ""}*/
+    ],              //Legg inn bildenavn med anførselstegn og komma mellom
+    youtube: "" 
+},
+{   dato: 2020,
+    tittel: "Grease",
+    innhold: "Avlyst pga. pandemien",
+    bildemappe: "Grease2020/",
+    bilder: [
+         /*{bildefil: "", bildetekst: "", bildekreditt: ""}*/
+    ],              //Legg inn bildenavn med anførselstegn og komma mellom
+    youtube: "" 
+},
+{   dato: 2019,
+    tittel: "Skatten på Sjørøverøya",
+    innhold: "",
+    bildemappe: "Skatten2019/",
+    bilder: [
+         /*{bildefil: "", bildetekst: "", bildekreditt: ""}*/
+    ],              //Legg inn bildenavn med anførselstegn og komma mellom
+    youtube: "" 
+},
+{   dato: 2018,
+    tittel: "Olsenbanden Jr. på Circus",
+    innhold: "",
+    bildemappe: "OlsenBanden2018/",
+    bilder: [
+        /* {bildefil: "", bildetekst: "", bildekreditt: ""}*/
+    ],              //Legg inn bildenavn med anførselstegn og komma mellom
+    youtube: "" 
+},
+{   dato: 2017,
+    tittel: "Aladdin",
+    innhold: "",
+    bildemappe: "Aladdin2017/",
+    bilder: [
+         /*{bildefil: "", bildetekst: "", bildekreditt: ""}*/
+    ],              //Legg inn bildenavn med anførselstegn og komma mellom
+    youtube: "" 
+},
+{   dato: 2016,
+    tittel: "Oliver",
+    innhold: "",
+    bildemappe: "Oliver2016/",
+    bilder: [
+         /*{bildefil: "", bildetekst: "", bildekreditt: ""}*/
+    ],              //Legg inn bildenavn med anførselstegn og komma mellom
+    youtube: "https://youtu.be/FXzxpgNG7lU?si=X51MNXPAqxJUPpLU" 
+},
+{   dato: 2015,
+    tittel: "Dalmatinerene",
+    innhold: "",
+    bildemappe: "Dalmatinerene2015/",
+    bilder: [
+         /*{bildefil: "", bildetekst: "", bildekreditt: ""}*/
+    ],              //Legg inn bildenavn med anførselstegn og komma mellom
+    youtube: "" 
+},
+{   dato: 2014,
+    tittel: "Peter Pan",
+    innhold: "",
+    bildemappe: "PeterPan2014/",
+    bilder: [
+         /*{bildefil: "", bildetekst: "", bildekreditt: ""}*/
+    ],              //Legg inn bildenavn med anførselstegn og komma mellom
+    youtube: "" 
+},
+{   dato: 2014,
+    tittel: "Peter Pan",
+    innhold: "",
+    bildemappe: "PeterPan2014/",
+    bilder: [
+         /*{bildefil: "", bildetekst: "", bildekreditt: ""}*/
+    ],              //Legg inn bildenavn med anførselstegn og komma mellom
+    youtube: "" 
+},
+{   dato: 2013,
+    tittel: "Annie",
+    innhold: "",
+    bildemappe: "Annie2013/",
+    bilder: [
+         /*{bildefil: "", bildetekst: "", bildekreditt: ""}*/
+    ],              //Legg inn bildenavn med anførselstegn og komma mellom
+    youtube: "" 
+},
+{   dato: 2012,
+    tittel: "Jungelboken",
+    innhold: "",
+    bildemappe: "Jungelboken2012/",
+    bilder: [
+         /*{bildefil: "", bildetekst: "", bildekreditt: ""}*/
+    ],              //Legg inn bildenavn med anførselstegn og komma mellom
+    youtube: "" 
+},
+{   dato: 2011,
+    tittel: "Grease",
+    innhold: "",
+    bildemappe: "Grease2011/",
+    bilder: [
+         /*{bildefil: "", bildetekst: "", bildekreditt: ""}*/
+    ],              //Legg inn bildenavn med anførselstegn og komma mellom
+    youtube: "" 
+},
+{   dato: 2010,
+    tittel: "Tollmannen fra Oz",
+    innhold: "",
+    bildemappe: "Trollmann2010/",
+    bilder: [
+         /*{bildefil: "", bildetekst: "", bildekreditt: ""}*/
+    ],              //Legg inn bildenavn med anførselstegn og komma mellom
+    youtube: "" 
+},
+{   dato: 2009,
+    tittel: "Bugsy Malone",
+    innhold: "",
+    bildemappe: "",
+    bilder: [
+         /*{bildefil: "", bildetekst: "", bildekreditt: ""}*/
+    ],              //Legg inn bildenavn med anførselstegn og komma mellom
+    youtube: "" 
+},
+{   dato: 2008,
+    tittel: "Ronja Røverdatter",
+    innhold: "",
+    bildemappe: "",
+    bilder: [
+         /*{bildefil: "", bildetekst: "", bildekreditt: ""}*/
+    ],              //Legg inn bildenavn med anførselstegn og komma mellom
+    youtube: "" 
+},
+{   dato: 2007,
+    tittel: "Aladdin",
+    innhold: "",
+    bildemappe: "",
+    bilder: [
+         /*{bildefil: "", bildetekst: "", bildekreditt: ""}*/
+    ],              //Legg inn bildenavn med anførselstegn og komma mellom
+    youtube: "" 
+},
+{   dato: 2006,
+    tittel: "Robin hood",
+    innhold: "",
+    bildemappe: "",
+    bilder: [
+         /*{bildefil: "", bildetekst: "", bildekreditt: ""}*/
+    ],              //Legg inn bildenavn med anførselstegn og komma mellom
+    youtube: "" 
+},
+{   dato: 2006,
+    tittel: "Ulven og geiteillingen",
+    innhold: "",
+    bildemappe: "",
+    bilder: [
+         /*{bildefil: "", bildetekst: "", bildekreditt: ""}*/
+    ],              //Legg inn bildenavn med anførselstegn og komma mellom
+    youtube: "" 
+},
+{   dato: 2005,
+    tittel: "Snehvit",
+    innhold: "",
+    bildemappe: "",
+    bilder: [
+         /*{bildefil: "", bildetekst: "", bildekreditt: ""}*/
+    ],              //Legg inn bildenavn med anførselstegn og komma mellom
+    youtube: "" 
+},
+{   dato: 2004,
+    tittel: "Skatten på sjørøverøya",
+    innhold: "",
+    bildemappe: "",
+    bilder: [
+         /*{bildefil: "", bildetekst: "", bildekreditt: ""}*/
+    ],              //Legg inn bildenavn med anførselstegn og komma mellom
+    youtube: "" 
+},
+{   dato: 2003,
+    tittel: "Jungelboken",
+    innhold: "",
+    bildemappe: "",
+    bilder: [
+         /*{bildefil: "", bildetekst: "", bildekreditt: ""}*/
+    ],              //Legg inn bildenavn med anførselstegn og komma mellom
+    youtube: "" 
+},
+{   dato: 2002,
+    tittel: "Pinocchio",
+    innhold: "",
+    bildemappe: "",
+    bilder: [
+         /*{bildefil: "", bildetekst: "", bildekreditt: ""}*/
+    ],              //Legg inn bildenavn med anførselstegn og komma mellom
+    youtube: "" 
+},
+{   dato: 2001,
+    tittel: "Tornerose",
+    innhold: "",
+    bildemappe: "",
+    bilder: [
+         /*{bildefil: "", bildetekst: "", bildekreditt: ""}*/
+    ],              //Legg inn bildenavn med anførselstegn og komma mellom
+    youtube: "" 
+},
+{   dato: 2000,
+    tittel: "Tornerose",
+    innhold: "",
+    bildemappe: "",
+    bilder: [
+         /*{bildefil: "", bildetekst: "", bildekreditt: ""}*/
+    ],              //Legg inn bildenavn med anførselstegn og komma mellom
+    youtube: "" 
+},
+{   dato: 1999,
+    tittel: "Prøysen kabaret",
+    innhold: "",
+    bildemappe: "",
+    bilder: [
+         /*{bildefil: "", bildetekst: "", bildekreditt: ""}*/
+    ],              //Legg inn bildenavn med anførselstegn og komma mellom
+    youtube: "" 
+},
+{   dato: 1998,
+    tittel: "Den levende skogen",
+    innhold: "",
+    bildemappe: "",
+    bilder: [
+         /*{bildefil: "", bildetekst: "", bildekreditt: ""}*/
+    ],              //Legg inn bildenavn med anførselstegn og komma mellom
+    youtube: "" 
+},
+{   dato: 1997,
+    tittel: "Nissene på villspor",
+    innhold: "",
+    bildemappe: "",
+    bilder: [
+         /*{bildefil: "", bildetekst: "", bildekreditt: ""}*/
+    ],              //Legg inn bildenavn med anførselstegn og komma mellom
+    youtube: "" 
 }
-
-
 ];
+
+/*
+
+<iframe width="560" height="315" src="https://www.youtube.com/embed/9YkW_9yIpvs?si=WKfl_pyKLFN0oLgx" 
+   title="YouTube video player" frameborder="0" allow="accelerometer; 
+   autoplay; clipboard-write; encrypted-media; gyroscope; 
+   picture-in-picture; web-share" 
+   referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
+</iframe>
+
+*/
